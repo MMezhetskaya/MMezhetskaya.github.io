c6ae8d2bca54ea5873def3eb40fcc23fc8e5c435
MMezhetskaya <1283@mail.ru>
2019-05-28 10:04:26 +0200

    video popup

---
+++ fruit/www/js/script.js
@@ -0,0 +1,25 @@
+jQuery(function($){
+	"use script";
+	$('.home_slider').slick({
+		vertical:true,
+		arrows:false,
+		dots:true
+	});
+	/*$('.open_video').magnificPopup({
+  		type: 'iframe'
+  		// other options
+	});*/
+
+     /* $(document).ready(function() {
+        $('.open_video').magnificPopup({
+          disableOn: 700,
+          type: 'iframe',
+          mainClass: 'mfp-fade',
+          removalDelay: 160,
+          preloader: false,
+
+          fixedContentPos: false
+        });
+      });*/
+    
+})
