baef843b25c52e4846412e3bc607f093058e2eb3
Maria Mezhetskaia <1283@mail.ru>
2019-05-30 20:53:36 +0200

    gallery

--- fruit/www/js/script.js
+++ fruit/www/js/script.js
@@ -32,5 +32,15 @@ jQuery(function($){
           fixedContentPos: false
       });
   }
+
+  if($('.testy_gallery').length){
+    $('.testy_gallery').magnificPopup({
+        type:'image',
+        closeOnContentClick:true,
+        mainClass:'mfp-img-mobile',
+        image:{verticalFit:true}
+    });
+  }
+
         
 });    
