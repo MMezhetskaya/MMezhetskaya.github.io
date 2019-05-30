2fbe9ff64b52d0a1175a9baf7ffaa44e26410336
MMezhetskaya <1283@mail.ru>
2019-05-30 21:46:26 +0200

    fix popup right block

--- fruit/www/js/script.js
+++ fruit/www/js/script.js
@@ -32,5 +32,28 @@ jQuery(function($){
           fixedContentPos: false
       });
   }
+
+  if($('.testy_gallery').length){
+     $('.testy_gallery').magnificPopup({
+          type: 'image',
+          closeOnContentClick: true,
+          mainClass: 'mfp-img-mobile',
+          image: {
+            verticalFit: true
+          }
+          
+        });
+  }
+
+   $('.image-popup-vertical-fit').magnificPopup({
+          type: 'image',
+          closeOnContentClick: true,
+          mainClass: 'mfp-img-mobile',
+          image: {
+            verticalFit: true
+          }
+          
+        });
+
         
 });    
